784eec58206faa37b5ad56f02351a8935f39c9bf
ismail <iamezian1@gmail.com>
2026-07-23 09:39:53 +0200

    Docker + Traefik configuratie voor VPS: health.agiledevops.nl

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,11 @@ const config = {
   kit: {
     adapter: adapter(),
     csrf: {
-      trustedOrigins: ['http://localhost:3000', 'http://127.0.0.1:3000']
+      trustedOrigins: [
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+        'https://health.agiledevops.nl'
+      ]
     }
   }
 };
